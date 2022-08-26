16abd7809e3aef49cdeb8dd2b31b347812b30523
andregra <andregra717@gmail.com>
2022-08-26 10:34:31 +0200

    add solution 2.5

--- part2/2.1-2.5/src/App.js
+++ part2/2.1-2.5/src/App.js
@@ -1,46 +1,4 @@
-const Header = (props) => {
-  return (
-    <h2>{props.course}</h2>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    parts.map((part) =>
-      <Part part={part.name} exercise={part.exercises} key={part.id} />
-    )
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p key={props.id}>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Total = ({ parts }) => {
-  const initialValue = 0
-  const sum = parts.reduce(
-    (previousValue, currentValue) => previousValue + currentValue.exercises
-    , initialValue
-  )
-  return (
-    <p style={{ fontWeight: "bold" }}>{`total of ${sum} ${sum > 1 ? "exercises" : "exercise"}`}</p>
-  )
-}
-
-const Course = (props) => {
-  const { course } = props
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const course = [
